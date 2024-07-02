be111f8b5e152270d36bc78c52a19d3840d4be0d
Attila Farkas <attila@webgurus.biz>
2024-07-02 10:29:02 +0300

    Page header, mobile fade in menu

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,5 +1,6 @@
 import domReady from '@roots/sage/client/dom-ready';
 import 'bootstrap';
+// import {ScrollPosStyler} from 'scrollpos-styler';
 
 /**
  * Application entrypoint
@@ -12,3 +13,39 @@ domReady(async () => {
  * @see {@link https://webpack.js.org/api/hot-module-replacement/}
  */
 if (import.meta.webpackHot) import.meta.webpackHot.accept(console.error);
+
+/**
+ * Navbar toggler with overlay menu and attach class menu-open to body
+ */
+const TOGGLER = document.querySelector('.navbar-toggler');
+
+TOGGLER.addEventListener('click', function (e) {
+  e.preventDefault();
+  TOGGLER.ariaExpanded = !JSON.parse(TOGGLER.ariaExpanded);
+
+  TOGGLER.setAttribute(
+    'aria-pressed',
+    TOGGLER.matches('[aria-pressed=true]') ? 'false' : 'true',
+  );
+
+  document.body.classList.toggle('menu-open');
+
+  /**
+   * Get navbar dropdown menus
+   */
+  const navbarDropdownMenus = document.querySelectorAll(
+    '.navbar-nav li.menu-item.dropdown ul.dropdown-menu',
+  );
+
+  // Remove the d-block class from the navbar dropdown menu
+  navbarDropdownMenus.forEach((menu) => {
+    if (menu.classList.contains('d-block')) {
+      menu.classList.remove('d-block');
+    }
+  });
+});
+
+/**
+ * Scrollpos styler
+ */
+// ScrollPosStyler;
--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,9 +1,21 @@
-<header class="header main-header py-4">
-  <section class="header__container container d-flex justify-content-between align-items-center">
-    <a class="brand navbar-brand" href="{{ $home_url }}" title="{{ $site_name }}">{!! $siteName !!}</a>
+<header class="main-header sps bg-white">
+  <nav class="nav-primary navbar navbar-expand-xl">
+    <div class="container">
+      <a class="brand navbar-brand py-0" href="{{ home_url('/') }}">
+        <img class="img-fluid header-logo" src="@asset('images/logo.svg')" alt="{{ get_bloginfo('name', 'display') }}" width="235" height="31"
+          loading="eager" />
+      </a>
 
-    <nav class="nav-primary navbar navbar-expand-xl" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      <div class="collapse navbar-collapse justify-content-lg-start" id="menu-collapse-main">
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#menu-collapse-main" type="button"
+        aria-controls="menu-collapse-main" aria-expanded="false" aria-label="Toggle navigation">
+        <svg aria-hidden="true" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none">
+          <rect width="18" height="1.5" fill=red ry="0.75" x="3" y="6.25" />
+          <rect width="18" height="1.5" fill=red ry="0.75" x="3" y="11.25" />
+          <rect width="18" height="1.5" fill=red ry="0.75" x="3" y="16.25" />
+        </svg>
+      </button>
+
+      <div class="navbar-collapse collapse" id="menu-collapse-main">
         {{-- @if (has_nav_menu('primary_navigation'))
           {!! wp_nav_menu([
               'theme_location' => 'primary_navigation',
@@ -17,28 +29,11 @@
         @if (has_nav_menu('primary_navigation'))
           {!! wp_nav_menu([
               'theme_location' => 'primary_navigation',
-              'menu_class' => 'nav navbar-nav mb-3 mb-xl-0 mx-auto',
+              'menu_class' => 'navbar-nav',
               'walker' => new \App\wp_bootstrap5_navwalker(),
           ]) !!}
         @endif
-
-        <div class="navigation-actions navbar-footer d-xl-none py-5">
-          <div class="header-icon-nav d-flex justify-content-end align-items-center gap-3">
-            <span class="header-search" id="mobile-search-trigger">
-              @svg('images.icons.icon-magnifying-glass')
-            </span>
-          </div>
-        </div>
       </div>
-
-      <button class="navbar-toggler collapsed" type="button" title="{{ __('mobile menu icon', 'sage') }}" data-bs-toggle="collapse"
-        data-bs-target="#menu-collapse-main" aria-controls="menu-collapse-main" aria-expanded="false" tabindex="1">
-        <svg width="24" height="24" aria-hidden="true" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none">
-          <rect width="18" height="1.5" fill=red ry="0.75" x="3" y="6.25" />
-          <rect width="18" height="1.5" fill=red ry="0.75" x="3" y="11.25" />
-          <rect width="18" height="1.5" fill=red ry="0.75" x="3" y="16.25" />
-        </svg>
-      </button>
-    </nav>
-  </section>
+    </div>
+  </nav>
 </header>
